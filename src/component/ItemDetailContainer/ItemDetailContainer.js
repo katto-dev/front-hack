4845155c437ad92d6ff63ace3f15636070017dd6
Bruno Rolon <leobrunorolon@gmail.com>
2022-03-18 12:21:05 -0300

    fix itemDetailContainer

--- src/component/ItemDetailContainer/ItemDetailContainer.js
+++ src/component/ItemDetailContainer/ItemDetailContainer.js
@@ -52,57 +52,50 @@ const ItemDetailContainer = () => {
       ) : data.length ? (
         data.map((item) => (
           <>
-            <div key={item._id}>
-              <div className="container proveedor-space-bottom" key={item._id}>
-                <div className="row pt-5 pb-4">
-                  <div className="col-12 col-md-8">
-                    <h1 className="text-aligcenter">{item.title}</h1>
-                  </div>
-                  <div className="col-12 col-md-4 cart">
-                    <Image src={item.img} width={"200"} />
-                  </div>
+            <div className="container proveedor-space-bottom" key={item._id}>
+              <div className="row pt-5 pb-4">
+                <div className="col-12 col-md-8">
+                  <h1 className="text-aligcenter">{item.title}</h1>
                 </div>
-                <div className="row pb-4">
-                  <div className="col-12">
-                    <p>{item.Description}</p>
-                  </div>
+                <div className="col-12 col-md-4 cart">
+                  <Image src={item.img} width={"200"} />
                 </div>
-                <div className="row pb-4">
-                  <div className="col-12">
-                    <span>{item.Url}</span>
-                  </div>
+              </div>
+              <div className="row pb-4">
+                <div className="col-12">
+                  <p>{item.description}</p>
                 </div>
-                <div className="row pb-4">
-                  <div className="col-12">
-                    <span>Licencias Disponibles: {item.stock}</span>
-                  </div>
+              </div>
+              <div className="row pb-4">
+                <div className="col-12">
+                  <span>{item.url}</span>
                 </div>
-                <div className="col-12 col-md-6 pb-2">
-                  <div className="row pb-4">
-                    <div className="col-12 col-md-6 pb-2">
-                      <Button
-                        className="bton bton-primary"
-                        onClick={onAdd}
-                        as={Link}
-                        to="/formulario"
-                      >
-                        Solicitar
-                      </Button>
-                      
-                    </div>
-                    <div className="col-12 pb-4">
-                      <Button
-                        className="bton bton-primary"
-                        as={Link}
-                        to="/licencias"
-                      >
-                        Volver
-                      </Button>
-                    </div>
-                  </div>
-                  
-                  
-                  
+              </div>
+              <div className="row pb-4">
+                <div className="col-12">
+                  <span>Licencias Disponibles: {item.stock}</span>
+                </div>
+              </div>
+
+              <div className="row pb-4">
+                <div className="col-12 col-md-6">
+                  <Button
+                    className="bton bton-primary"
+                    onClick={onAdd}
+                    as={Link}
+                    to="/formulario"
+                  >
+                    Solicitar
+                  </Button>
+                </div>
+                <div className="col-12 col-md-6">
+                  <Button
+                    className="bton bton-primary"
+                    as={Link}
+                    to="/licencias"
+                  >
+                    Volver
+                  </Button>
                 </div>
               </div>
             </div>
